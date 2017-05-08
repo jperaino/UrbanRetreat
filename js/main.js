6b62f29cb4c7e03d437724ed984a3f9806847770
jperaino <jprn72@gmail.com>
2017-05-07 22:11:07 -0400

    add google fonts

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		scene.background = new THREE.Color( 0x90caf9 );
 
 		var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		camera.position.set(0,0,10);
+		camera.position.set(0,0,1);
 
 		var renderer = new THREE.WebGLRenderer({ antialias: true });
 		renderer.setSize( window.innerWidth, window.innerHeight);
@@ -41,7 +41,7 @@ $(document).ready(function(){
 
 		// Add a cube
 		var geometry = new THREE.BoxGeometry(.75,.75,.75);
-		var material = new THREE.MeshBasicMaterial({color: 0x5d99c6});
+		var material = new THREE.MeshBasicMaterial({color: 0xffffff});
 		var cube = new THREE.Mesh(geometry, material);
 		scene.add( cube );
 
@@ -97,9 +97,9 @@ $(document).ready(function(){
 			requestAnimationFrame( render );
 
 				// Set cube rotation
-				cube.rotation.x += 0.01
-				cube.rotation.y += 0.01
-				cube.rotation.z += 0.01
+				cube.rotation.x += 0.001
+				cube.rotation.y += 0.001
+				cube.rotation.z += 0.001
 
 				// Render scene
 				renderer.render( scene, camera);
